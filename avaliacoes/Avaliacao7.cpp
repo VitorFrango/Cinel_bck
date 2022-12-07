e9fd5f6a98198a7a475ffcd56450167a2f3bcadf
VitorFrango <51138494+VitorFrango@users.noreply.github.com>
2022-12-07 20:29:11 +0000

    Initial commit

--- avaliacoes/Avaliacao7.cpp
+++ avaliacoes/Avaliacao7.cpp
@@ -4,61 +4,68 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <limits>
+
 
 using namespace std;
 
 class Escola {
 private:
-    int             idEscola;
-    char            localidade;
-    vector<string>  nomeEscola;
-    vector<string>  turmas;
+    string                  NomeEscola;
+    int                     idEscola;
+    string                  localidade;
+    vector <string>         Turmas;
 
 public:
+    Escola(int novoIdEscola, string novoNomeEscola, string novaLocalidade, vector<string> novaTurma)
+            : idEscola(novoIdEscola), NomeEscola(novoNomeEscola), localidade(novaLocalidade), Turmas(novaTurma) {}
 
-    Escola(int novoIdEscola, char novoNomeEscola, char novaLocalidade, vector<string> novasTurmas)
-            : idEscola(novoIdEscola), nomeEscola(novoNomeEscola), localidade(novaLocalidade), turmas(novasTurmas) {}
-
-
-    int getIdEscola() {
-        return this->idEscola;
-    }
-
-    char getLocalidade() {
-        return this->localidade;
-    }
 
     void setIdEscola(int novoIdEscola) {
         this->idEscola = novoIdEscola;
     }
 
+    int getIdEscola() {
+        return this->idEscola;
+    }
 
-    void setLocalidade(char novaLocalidade) {
+    string getNomeEscola (){
+        return this -> NomeEscola;
+    }
+
+    void setNomeEscola(string NomeEscola){
+        this-> NomeEscola;
+    }
+
+
+    void setLocalidade(string novaLocalidade) {
         this->localidade = novaLocalidade;
     }
 
-    void setTurmas(vector<string>novasTurmas){
-        this-> turmas = novasTurmas;
+    string getLocalidade() {
+        return this-> localidade;
     }
 
-    void setnomeEscola(vector<string>novonomeEscola){
-        this-> nomeEscola = novonomeEscola;
+    void setTurmas(vector<string>novaTurma){
+        this-> Turmas;
+        Turmas = novaTurma;
+    }
+    vector<string>getTurma(){
+        return  this -> Turmas;
     }
 
-    vector<string>getTurmas(){
-        return  this -> turmas;
-    }
 
     void adicionarTurma(string novaTurma){
-        this -> turmas.push_back(novaTurma);
+        this -> Turmas.push_back(novaTurma);
     }
 
     bool removerTurma(string turma) {
-        int tamanho = this->turmas.size();
+        int tamanho = this->Turmas.size();
 
         for (int i = 0; i < tamanho; i++) {
-            if (this->turmas[i] == turma) {
-                this->turmas.erase(this->turmas.begin() + i);
+            if (this->Turmas[i] == turma) {
+                this->Turmas.erase(this->Turmas.begin() + i);
                 return true;
             }
         }
@@ -66,99 +73,75 @@ public:
         return false;
     }
 
-    vector<string>getnomeEscola(){
-        return  this -> nomeEscola;
-    }
 
-    void adicionarnomeEscola(string novonomeEscola){
-        this -> nomeEscola.push_back(novonomeEscola);
-    }
-
-    bool removenomeEscola(string nomeEscola) {
-        int tamanho = this->nomeEscola.size();
-
-        for (int i = 0; i < tamanho; i++) {
-            if (this->nomeEscola[i] == nomeEscola) {
-                this->nomeEscola.erase(this->nomeEscola.begin() + i);
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     string listaTurmas() {
-        int tamanho = this->turmas.size();
-        string turmasString = "";
+        string TurmasString = "";
+
+        for (int i = 0; i < Turmas.size(); i++) {
+            TurmasString += this->Turmas[i] + ", ";
+        }
+
+        return TurmasString;
+    }
+
+    string listaNomeEscola() {
+        int tamanho = this->NomeEscola.size();
+        string NomeEscolaString = "";
 
         for (int i = 0; i < tamanho; i++) {
-            turmasString += this->turmas[i] + ", ";
+            NomeEscolaString += this->NomeEscola[i] + ", ";
         }
 
-        return turmasString;
-        }
-
-        string listanomeEscola() {
-            int tamanho = this->nomeEscola.size();
-            string nomeEscolaString = "";
-
-            for (int i = 0; i < tamanho; i++) {
-                nomeEscolaString += this->nomeEscola[i] + ", ";
-            }
-
-            return nomeEscolaString;
-        }
+        return NomeEscolaString;
+    }
 
 
 };
 
 class Turma {
 private:
-    int             IdTurma;
-    int             ano;
-    vector<string>  aluno;
+    int                 IdTurma;
+    int                 ano;
+    vector<string>      Aluno;
 
 public:
 
     Turma(int novoIdTurma, int novoAno, vector<string> novoAluno)
-            : IdTurma(novoIdTurma), ano(novoAno), aluno(novoAluno) {}
+            : IdTurma(novoIdTurma), ano(novoAno), Aluno(novoAluno) {}
 
 
-    int getidturma() {
+
+    int getIdTurma() {
         return this->IdTurma;
     }
 
-    char getAno() {
+    void setAno(unsigned int  novoAno) {
+        this->ano = novoAno;
+    }
+    int getAno() {
         return this->ano;
     }
 
 
-    void setIdTurma(int novoIdTurma) {
-        this->IdTurma = novoIdTurma;
-    }
-
-    void setAno(char novoAno) {
-        this->ano = novoAno;
-    }
-
     void setAluno(vector<string>novoAluno){
-        this-> aluno = novoAluno;
+        this-> Aluno = novoAluno;
     }
 
     vector<string>getAluno(){
-        return  this -> aluno;
+        return  this -> Aluno;
     }
 
     void adicionarAluno(string novoAluno){
-        this -> aluno.push_back(novoAluno);
+        this -> Aluno.push_back(novoAluno);
     }
 
-    bool removerAluno(string aluno) {
-        int tamanho = this->aluno.size();
+    bool removeAluno(string Aluno) {
+        int tamanho = this->Aluno.size();
 
         for (int i = 0; i < tamanho; i++) {
-            if (this->aluno[i] == aluno) {
-                this->aluno.erase(this->aluno.begin() + i);
+            if (this->Aluno[i] == Aluno) {
+                this->Aluno.erase(this->Aluno.begin() + i);
                 return true;
             }
         }
@@ -166,137 +149,242 @@ public:
         return false;
     }
 
-    string listaAluno() {
-        int tamanho = this->listaAluno().size();
-        string alunoString = "";
+    vector<string> getaluno(){
+        return  this -> Aluno;
+    }
+
+    void adicionaAluno(string novoAluno){
+        this -> Aluno.push_back(novoAluno);
+    }
+
+    bool removerAluno(string aluno) {
+        int tamanho = this->Aluno.size();
 
         for (int i = 0; i < tamanho; i++) {
-            alunoString += this->listaAluno()[i] + ", ";
+            if (this->Aluno[i] == aluno) {
+                this->Aluno.erase(this->Aluno.begin() + i);
+                return true;
+            }
         }
 
-        return alunoString;
+        return false;
     }
+
+    string listaAlunos() {
+        int tamanho = this->Aluno.size();
+        string AlunosString = "";
+
+        for (int i = 0; i < tamanho; i++) {
+            AlunosString += this->Aluno[i] + ", ";
+        }
+
+        return AlunosString;
+    }
+
+    string listanomeEscola() {
+        int tamanho = this->Aluno.size();
+        string AlunosString = "";
+
+        for (int i = 0; i < tamanho; i++) {
+            AlunosString += this->Aluno[i] + ", ";
+        }
+
+        return AlunosString;
+    }
+
 
 };
 
 class Aluno {
+private:
+    string  nome;
+    int     cc;
+
+
 public:
-    string nome;
-    int Cc ;
-
-    Aluno(string nome, int Cc) {
-        this->setNome(nome);
-        this->setCc(Cc);
-    }
+    Aluno(string novoNome, int NovoCc)
+            : nome(novoNome), cc(NovoCc) {}
 
 
-    void setNome(string nome) {
-        this->nome = nome;
+
+    void setNome(string novoNome) {
+        this->nome = novoNome;
     }
 
     string getNome() {
         return this->nome;
     }
 
-
-    void setCc(int Cc) {
-        this->Cc = Cc;
+    void setCc(int NovoCc) {
+        this -> cc = NovoCc;
     }
 
     int getCc() {
-        return this->Cc;
+        return this-> cc;
+    }
+};
 
 
-    void menuEscola(Escola escola) {
-        int input;
-        string inputString;
-        cout << "Escolha uma Opção:" <<                             endl;
-        cout << "\t[1] -    Adicionar uma escola" <<                endl;
-        cout << "\t[2] -    Eliminar  uma escola" <<                endl;
-        cout << "\t[3] -    Editar os atributos de uma escola" <<   endl;
-        cout << "\t[4] -    Listar as turmas de uma escola" <<      endl;
-        cout << "\t[5] -    Criar uma nova turma" <<                endl;
-        cout << "\t[6] -    Eliminar uma turma" <<                  endl;
-        cout << "\t[7] -    Editar uma turma" <<                    endl;
-        cout << "\t[8] -    Listar os alunos de uma turma" <<       endl;
-        cout << "\t[9] -    Criar um novo aluno" <<                 endl;
-        cout << "\t[10] -   Eliminar um aluno" <<                   endl;
-        cout << "\t[11] -   Editar um aluno" <<                     endl;
-        cout << endl << "\t[0] - Sair do Menu" <<                   endl;
+int main () {
+
+    vector<Escola> escolas;
+    vector<Turma> turmas;
+    vector<Aluno> alunos;
+
+    bool continuar = true;
+    int input;
+    string inputString;
+
+    while (continuar) {
+
+
+        cout << "Escolha uma Opção:" << endl;
+        cout << "\t[1] -    Adicionar uma escola" << endl;
+        cout << "\t[2] -    Eliminar  uma escola" << endl;
+        cout << "\t[3] -    Editar os atributos de uma escola" << endl;
+        cout << "\t[4] -    Listar as turmas de uma escola" << endl;
+        cout << "\t[5] -    Criar uma nova turma" << endl;
+        cout << "\t[6] -    Eliminar uma turma" << endl;
+        cout << "\t[7] -    Editar uma turma" << endl;
+        cout << "\t[8] -    Listar os alunos de uma turma" << endl;
+        cout << "\t[9] -    Criar um novo aluno" << endl;
+        cout << "\t[10] -   Eliminar um aluno" << endl;
+        cout << "\t[11] -   Editar um aluno" << endl;
+        cout << endl << "\t[0] - Sair do Menu" << endl;
         cin >> input;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
 
         switch (input) {
-            case 0:
+            case 0: {
+                continuar = false;
                 break;
-            case 1:
+            }
+
+            case 1: {
+                string novoNomeEscola;
+                string novaLocalidade;
+                vector<string> novaTurma;
+                int novoIdEscola;
+
                 cout << "Por favor, escreva o nome da escola a adicionar:" << endl;
-                getline(cin, inputString);
-                escola.adicionarnomeEscola(inputString);
+                getline(cin, novoNomeEscola);
+
+                cout << " Escreva a lovcalidae" << endl;
+                getline(cin, novaLocalidade);
+
+                cout << " introdza o Id da escola" << endl;
+                cin >> novoIdEscola;
+
+
+                Escola novaEscola(novoIdEscola, novoNomeEscola, novaLocalidade, novaTurma);
+                escolas.push_back(novaEscola);
+
                 break;
-            case 2:
+            }
+
+            case 2: {
                 cout << "Por favor, escreva o nome da escola a eliminar:" << endl;
                 getline(cin, inputString);
-                escola.removenomeEscola(inputString);
-                break;
 
-            case 3:
+                break;
+            }
+
+            case 3: {
                 cout << "Editar os atributos de uma escola:" << endl;
                 break;
+            }
 
-            case 4:
-                cout << "Lista de turmas de uma escols" << escola.listaTurmas() << ":" << endl;
-                //getline(cin, inputString);
-                cout << escola.listaTurmas();
+            case 4: {
+                cout << "Lista de turmas de uma escola" << ":" << endl;
+                string nomeescola;
+                cout << "Qual é a escola " << endl;
+                cin >> nomeescola;
+
+                for (int i = 0; i < escolas.size(); i++) {
+                    Escola item = escolas[i];
+                    if (nomeescola == item.getNomeEscola()) {
+                        cout << "Lista de turmas" << ":" << endl;
+                        cout << item.listaTurmas();
+                        break;
+                    }
+                }
                 break;
 
-            case 5:
-                cout << "Criar uma nova turma" << ":" << endl;
-                getline(cin, inputString);
-                escola.adicionarTurma(inputString);
+            }
+                case 5: {
+                    int novoIdTurma;
+                    int novoAno;
+                    vector<string> novaTurma;
 
-                break;
+                    cout << "Escrva o Id  nova turma" << ":" << endl;
+                    cin >> novoIdTurma;
 
-            case 6:
-                cout << "Eliminar uma  turma " << ":" << endl;
-                getline(cin, inputString);
-                escola.removerTurma(inputString);
-                break;
-
-            case 7:
-                cout << "Editar uma  turma " << ":" << endl;
-                
-                break;
-
-            case 8:
-                cout << "Listar os alunos de uma turma" << ":" << endl;
-
-                break;
-
-            case 9:
-                cout << "Criar um novo aluno" << ":" << endl;
-                getline(cin, inputString);
+                    cout << "Escreva o ano da turma " << ":" << endl;
+                    cin >> novoAno;
 
 
-                break;
+                    Turma turmas(novoIdTurma, novoAno, novaTurma);
 
-            case 10:
-                cout << "Eliminar um aluno " << ":" << endl;
-                getline(cin, inputString);
+                    break;
+                } // check
 
-                break;
+                case 6: {
+                    cout << "Eliminar uma  turma " << ":" << endl;
+                    getline(cin, inputString);
+                    break;
+                }
 
-            case 11:
-                cout << "Editar uma aluno " << ":" << endl;
+                case 7: {
+                    cout << "Editar uma  turma " << ":" << endl;
+                    break;
+                }
 
-                break;
+                case 8: {
+                    cout << "Listar os alunos de uma turma" << ":" << endl;
+
+                    for (int i = 0; i < alunos.size(); i++) {
+                        Aluno item = alunos[i];
+                        if(Aluno == item.nome()) {
+                            cout << "Lista de alunos" << ":" << endl;
+                            cout << item.getNome();
+                            break;
+                        }
+                    }
+                    break;
+
+
+                }
+
+                case 9: {
+                    int Cc;
+                    string nome;
+
+                    cout << "Escreva o Cartão de cidadão do aluno " << ":" << endl;
+                    cin >> Cc;
+
+                    cout << "Escreva o nome do aluno " << ":" << endl;
+                    cin >> nome;
+
+                    Aluno alunos(int Cc, string nome);
+                    break;
+                }
+
+                case 10: {
+                    cout << "Eliminar um aluno " << ":" << endl;
+                    getline(cin, inputString);
+                    break;
+                }
+
+                case 11: {
+                    cout << "Editar uma aluno " << ":" << endl;
+                    break;
+                }
+
+
+            }
 
         }
 
-    };
+    }
 
-int main (){
-
-
-}
